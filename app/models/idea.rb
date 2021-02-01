dc241a79ed4fcbf3df5ba52b83c369606bf8edad
Sheelove Dol <siesel26@gmail.com>
2021-02-01 15:50:37 -0800

    Add user model and authentication

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,4 +1,5 @@
 class Idea < ApplicationRecord
+  belongs_to :user, optional: true
   validates :title, presence: true, uniqueness: true
   validates :description, presence: true, length: { minimum: 20 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'ideas#index'
   resources :ideas
+
+  resources :users, only: [:new, :create]
+  resource :session, only: [:new, :create, :destroy]
+  
 end
